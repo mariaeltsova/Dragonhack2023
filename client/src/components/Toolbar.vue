0e085f0a5e0b1d03e55827469b497ad67c81d698
Jan Merhar <merharjan@gmail.com>
2023-05-13 22:19:49 +0200

    add toolbar

--- client/src/components/Toolbar.vue
+++ client/src/components/Toolbar.vue
@@ -1,68 +1,10 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-secondary">
-    <div class="container-fluid">
-      <a href="#" class="navbar-brand"></a>
-      <button
-        type="button"
-        class="navbar-toggler"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarCollapse"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <!-- Neprijavljen uporabnik -->
-        <div class="navbar-nav">
-          <a
-            href="#"
-            class="nav-item nav-link active"
-            @click="this.$router.push({ name: 'home' })"
-            >Opravila</a
-          >
-        </div>
-        <!-- Neprijavljen uporabnik -->
-        <div class="navbar-nav">
-          <a
-            href="#"
-            class="nav-item nav-link active"
-            @click="this.$router.push({ name: 'iskanje' })"
-            >Iskanje uporabnikov</a
-          >
-        </div>
-        <div class="navbar-nav">
-          <a
-            href="#"
-            class="nav-item nav-link active"
-            @click="this.$router.push({ name: 'sledilci' })"
-            >Sledilci</a
-          >
-        </div>
-        <!-- Neprijavljen uporabnik -->
-        <div class="navbar-nav">
-          <a
-            href="#"
-            class="nav-item nav-link active"
-            @click="this.$router.push({ name: 'napaka' })"
-            >Prijavi napako</a
-          >
-        </div>
-        <!-- Admin -->
-        <!-- Analitik -->
-        <!-- Razvijalec  -->
-        <!--  -->
-        <div class="navbar-nav ms-auto">
-          <a
-            href="#"
-            class="nav-item nav-link"
-            @click.prevent="this.$router.push({ name: 'prijava' })"
-            >Prijava / odjava</a
-          >
-        </div>
-        <!--  -->
-      </div>
-    </div>
+  <nav class="navbar fixed-bottom navbar-light bg-light">
+    <a class="navbar-brand" href="#">Bottom Navigation</a>
+    <a class="nav-link" href="#">Home</a>
+    <a class="nav-link" href="#">About</a>
+    <a class="nav-link" href="#">Contact</a>
   </nav>
-  <!--  -->
 </template>
 
 <style scoped></style>
