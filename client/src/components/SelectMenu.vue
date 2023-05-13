29539e3894cc0c00ada2a7cf19a5ebec353c4e52
Jan Merhar <merharjan@gmail.com>
2023-05-13 22:08:06 +0200

    add selectmenu.vue

---
+++ client/src/components/SelectMenu.vue
@@ -0,0 +1,26 @@
+<template>
+  <select class="form-select" :aria-label="label">
+    <option selected>{{ label }}</option>
+    <template v-for="option in options">
+      <option :value="option.value" @click="$emit('update', option.value)">
+        {{ option.text }}
+      </option>
+    </template>
+  </select>
+</template>
+
+<script>
+export default {
+  props: {
+    options: {
+      type: Array,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+  emits: ["update"],
+}
+</script>
