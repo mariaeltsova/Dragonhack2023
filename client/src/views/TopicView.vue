e1010b817eb746641f478d61090a8218eb7997f0
Jan Merhar <merharjan@gmail.com>
2023-05-13 22:37:09 +0200

    add topicview.vue

---
+++ client/src/views/TopicView.vue
@@ -0,0 +1,34 @@
+<script setup>
+import SelectMenu from "../components/SelectMenu.vue"
+import InputField from "../components/InputField.vue"
+</script>
+
+<template>
+  <select-menu
+    :label="'Izberi jezik'"
+    :options="languages"
+    @update="
+      (selected) => {
+        language = selected
+      }
+    "
+  ></select-menu>
+  <input-field
+    :label="'123'"
+    :placeholder="'vnosno polje'"
+    :type="'password'"
+  ></input-field>
+</template>
+
+<script>
+import { languages } from "../util/languages"
+
+export default {
+  data() {
+    return {
+      languages: languages,
+      language: "",
+    }
+  },
+}
+</script>
