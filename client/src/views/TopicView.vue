589c10a3a00323de2e9115fc8c3de2793691bd30
Jan Merhar <merharjan@gmail.com>
2023-05-14 00:22:21 +0200

    add topicview.vue

--- client/src/views/TopicView.vue
+++ client/src/views/TopicView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import SelectMenu from "../components/SelectMenu.vue"
 import InputField from "../components/InputField.vue"
+import TextArea from "../components/TextArea.vue"
 </script>
 
 <template>
+  <br />
   <select-menu
-    :label="'Izberi jezik'"
+    :label="'Choose language'"
     :options="languages"
     @update="
       (selected) => {
@@ -13,21 +15,53 @@ import InputField from "../components/InputField.vue"
       }
     "
   ></select-menu>
+  <br />
+  <select-menu
+    :label="'Select diffculty'"
+    :options="difficulties"
+    @update="
+      (selected) => {
+        difficulty = selected
+      }
+    "
+  ></select-menu>
+  <br />
   <input-field
     :label="'123'"
     :placeholder="'vnosno polje'"
     :type="'password'"
   ></input-field>
+
+  <br />
+
+  <text-area
+    :label="'Mark the words that you don\'t understand - they will be added to your vocabulary'"
+    :text="lorem"
+    @update="
+      (el) => {
+        markedWords = el
+      }
+    "
+  ></text-area>
+
+  <br />
+  <button type="submit" class="btn btn-secondary float-end">Save words</button>
 </template>
 
 <script>
 import { languages } from "../util/languages"
+import { diffuculties } from "../util/difficulties"
 
 export default {
   data() {
     return {
       languages: languages,
       language: "",
+      difficulties: diffuculties,
+      difficulty: "",
+      lorem:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      markedWords: [],
     }
   },
 }
