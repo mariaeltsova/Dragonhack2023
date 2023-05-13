306a858e703a654912bd8f572956c212a8b1ff3f
Jan Merhar <merharjan@gmail.com>
2023-05-13 19:13:27 +0200

    add googlebooks.js

---
+++ server/classes/GoogleBooks.js
@@ -0,0 +1,20 @@
+const gbooks = require("@chewhx/google-books")
+
+module.exports = class GoogleBooks {
+  async getBooksBySubject(subject) {
+    const result = await gbooks.standard(
+      { subject: subject },
+      { maxResults: 10 }
+    )
+    console.log(result)
+
+    return result
+  }
+
+  async getBooksByKeywords(keywords) {
+    const result = await gbooks.standard({}, { q: keywords, maxResults: 10 })
+    console.log(result)
+
+    return result
+  }
+}
