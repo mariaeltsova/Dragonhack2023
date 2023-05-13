c8e6f106f1e86a9537fd5fada8a1a42a12c55af5
Jan Merhar <merharjan@gmail.com>
2023-05-13 22:37:26 +0200

    update selectmenu

--- client/src/components/SelectMenu.vue
+++ client/src/components/SelectMenu.vue
@@ -2,8 +2,8 @@
   <select class="form-select" :aria-label="label">
     <option selected>{{ label }}</option>
     <template v-for="option in options">
-      <option :value="option.value" @click="$emit('update', option.value)">
-        {{ option.text }}
+      <option :value="option.code" @click="$emit('update', option.code)">
+        {{ option.name }}
       </option>
     </template>
   </select>
