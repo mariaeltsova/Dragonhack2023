bca3e85e1e24c35dc3400daf8760d7e4075d6fb3
Jan Merhar <merharjan@gmail.com>
2023-05-13 22:10:19 +0200

    add iputfield

---
+++ client/src/components/InputField.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="form-group">
+    <label for="exampleInputEmail1">{{ label }}</label>
+    <input :type="type" class="form-control" :placeholder="placeholder" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "InputField",
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    placeholder: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      default: "text",
+    },
+  },
+}
+</script>
