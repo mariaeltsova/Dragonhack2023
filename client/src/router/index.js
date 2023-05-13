d16246420570e660c3f25784d903d3a3bad8e9b2
Jan Merhar <merharjan@gmail.com>
2023-05-13 22:10:29 +0200

    add index.js

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router"
+import TopicView from "../views/TopicView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: "/topic",
+      name: "topic",
+      component: TopicView,
+    },
+  ],
 })
 
 export default router
