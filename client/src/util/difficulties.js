b6a96060ee274f4e202d326c7106e7c7c8a4dec8
Jan Merhar <merharjan@gmail.com>
2023-05-13 23:09:02 +0200

    add difficulties.js

---
+++ client/src/util/difficulties.js
@@ -0,0 +1,18 @@
+export const diffuculties = [
+  {
+    code: 1,
+    name: "Lower intermediate",
+  },
+  {
+    code: 2,
+    name: "Intermediate",
+  },
+  {
+    code: 3,
+    name: "Upper intermediate",
+  },
+  {
+    code: 4,
+    name: "Expert",
+  },
+]
