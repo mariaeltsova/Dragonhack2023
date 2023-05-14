7e73776f2dd8278cfbd8b0b14ae307c4b80ba8b1
Jan Merhar <merharjan@gmail.com>
2023-05-14 03:48:04 +0200

    add topbar.vue

--- client/src/components/TopBar.vue
+++ client/src/components/TopBar.vue
@@ -4,14 +4,12 @@
       <a class="navbar-brand" href="#"></a>
       <button
         class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
         data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent"
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span class="navbar-toggler-icon"></span>
+        <img src="../assets/user.png" class="profile" alt="" srcset="" />
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
@@ -26,3 +24,10 @@
     </div>
   </nav>
 </template>
+
+<style>
+.profile {
+  width: 5rem;
+  height: 5rem;
+}
+</style>
