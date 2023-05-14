f27e8fe743cf7f537b0a57927cfdc1e83f12a834
Jan Merhar <merharjan@gmail.com>
2023-05-14 02:54:13 +0200

    flashcard

---
+++ client/src/components/FlashCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <h3 class="text-center">Keyword</h3>
+  <br />
+  <button type="button" class="btn btn-primary btn-lg btn-block btn-full">
+    Check me
+  </button>
+
+  <div class="continer-fluid">
+    <br />
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem nostrum
+      dolore debitis ipsam animi sed obcaecati id quod perspiciatis adipisci?
+      Commodi asperiores fugiat ipsa alias voluptas, quisquam aperiam corporis
+      ullam.
+    </div>
+    <br />
+    <br />
+    <div class="row">
+      <div class="col-1"></div>
+      <div class="col-3">
+        <button type="button" class="btn btn-primary">&lt;</button>
+      </div>
+      <div class="col"></div>
+      <div class="col-3">
+        <button type="button" class="btn btn-primary">&gt;</button>
+      </div>
+    </div>
+    <br />
+  </div>
+</template>
+
+<style>
+.btn-full {
+  width: 100%;
+}
+</style>
