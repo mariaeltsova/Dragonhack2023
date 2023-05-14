f883dd8683a4c0503f50bbf5eea1bdce3f8f3fef
Jan Merhar <merharjan@gmail.com>
2023-05-14 03:56:13 +0200

    add toolbar

--- client/src/components/Toolbar.vue
+++ client/src/components/Toolbar.vue
@@ -1,10 +1,29 @@
 <template>
-  <nav class="navbar fixed-bottom navbar-light bg-light">
-    <a class="navbar-brand" href="#">Bottom Navigation</a>
-    <a class="nav-link" href="#">Home</a>
-    <a class="nav-link" href="#">About</a>
-    <a class="nav-link" href="#">Contact</a>
+  <nav class="navbar fixed-bottom navbar-light bg-light nav-style">
+    &nbsp;
+    <a class="nav-link" href="#">
+      <img src="../assets/card.png" class="profile" alt="" srcset="" />
+    </a>
+    <a class="nav-link" href="#">
+      <img src="../assets/document.png" class="profile" alt="" srcset="" />
+    </a>
+    <a class="nav-link" href="#">
+      <img src="../assets/puzzle.png" class="profile" alt="" srcset="" />
+    </a>
+    &nbsp;
   </nav>
 </template>
 
-<style scoped></style>
+<style scoped>
+.profile {
+  width: 4rem;
+  height: 4rem;
+}
+
+.nav-style {
+  background-color: #f8f9fa !important;
+  border-radius: 1rem;
+  padding: 0.5rem;
+  margin: 0.5rem;
+}
+</style>
