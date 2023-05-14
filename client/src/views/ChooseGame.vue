d89b12d25067987b2a15347dfee0798e957f3864
Jan Merhar <merharjan@gmail.com>
2023-05-14 07:01:32 +0200

    add choosegame

---
+++ client/src/views/ChooseGame.vue
@@ -0,0 +1,30 @@
+<template>
+  <br />
+  <button type="button" class="btn btn-primary btn-lg btn-block" @click="">
+    Correct a mistake
+  </button>
+  <br />
+  <br />
+  <button
+    type="button"
+    class="btn btn-primary btn-lg btn-block"
+    @click="this.$router.push({ name: 'complete' })"
+  >
+    Complete a word in sentance
+  </button>
+  <br />
+  <br />
+  <button
+    type="button"
+    class="btn btn-primary btn-lg btn-block"
+    @click="this.$router.push({ name: 'puzzle' })"
+  >
+    Solve the puzzle
+  </button>
+</template>
+
+<style scoped>
+button {
+  width: 100%;
+}
+</style>
