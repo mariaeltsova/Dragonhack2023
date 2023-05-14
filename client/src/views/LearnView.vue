1fe2fd5f1164d76e36e83a5765449f9ade22e790
Jan Merhar <merharjan@gmail.com>
2023-05-14 02:54:22 +0200

    learnview.vue

---
+++ client/src/views/LearnView.vue
@@ -0,0 +1,34 @@
+<script setup>
+import SelectMenu from "../components/SelectMenu.vue"
+import FlashCard from "../components/FlashCard.vue"
+</script>
+
+<template>
+  <h3 class="text-center">Learn your words</h3>
+  <br />
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col">
+        <select-menu :label="'Select mode'" :options="modes"></select-menu>
+      </div>
+      <div class="col-3">
+        <button type="submit" class="btn btn-secondary">Begin</button>
+      </div>
+    </div>
+  </div>
+  <br />
+  <br />
+  <flash-card></flash-card>
+</template>
+
+<script>
+import { modes } from "../util/modes"
+
+export default {
+  data() {
+    return {
+      modes: modes,
+    }
+  },
+}
+</script>
